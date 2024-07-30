80510120511637cc88ef19b918c9cecd0ae343d0
MellGriffin <missmelissagriffin@gmail.com>
2024-07-30 17:26:22 -0500

    made post route in foods.js

--- controllers/foods.js
+++ controllers/foods.js
@@ -17,4 +17,17 @@ router.get('/new', (req, res) => {
     res.render('foods/new.ejs');
 });
 
+//POST /foods create route
+router.post('/', async (req, res) => {
+try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.foods.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+} catch (error) {
+    console.log(error);
+    res.redirect('/')
+}
+});
+
 module.exports = router;
