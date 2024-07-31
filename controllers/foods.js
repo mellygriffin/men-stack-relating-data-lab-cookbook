0e5756b6c2dfb973774641175753e1c252308e2b
MellGriffin <missmelissagriffin@gmail.com>
2024-07-31 16:20:43 -0500

    Delete route created

--- controllers/foods.js
+++ controllers/foods.js
@@ -53,6 +53,19 @@ router.get('/:foodId', async (req, res) => {
     }
 });
 
+//DELETE post route
+router.delete('/:foodId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.id(req.params.foodId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
+});
+
 
 
 
